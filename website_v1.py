04e4e370117e0c920e3498d45a118a5596f02216
Jihyeon Jeong <76680663+jjh6838@users.noreply.github.com>
2024-02-17 23:09:17 +0900

    website_v1.py

--- website_v1.py
+++ website_v1.py
@@ -1,10 +1,21 @@
 import streamlit as st
 view = [100, 150, 30]
-st.write('# Ji test homepage')
-st.write('# Welcome to my homepage')
-st.write('## Test bar chart')
-st.bar_chart(view)
+st.write('# Test Homepage of Jihyeon Jeong (Ji)')
+st.write('## Welcome to my homepage')
+st.write('### My name is Jihyeon Jeong and I am working as a data and knowledge management specialist at the Green Climate Fund. You will be able to see my academic and professional work here.')
 
 import pandas as pd
-sview = pd.Series(view)
-sview
+import numpy as np
+
+df = pd.DataFrame({
+    "col1": np.random.randn(1000) / 50 + 37.76,
+    "col2": np.random.randn(1000) / 50 + -122.4,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist(),
+})
+
+st.map(df,
+    latitude='col1',
+    longitude='col2',
+    size='col3',
+    color='col4')
